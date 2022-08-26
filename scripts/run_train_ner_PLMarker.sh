695379652be51a40c27cd5b8ad5b650db16000bd
dijanasazdevska <67866541+dijanasazdevska@users.noreply.github.com>
2022-08-26 16:48:31 +0200

    Changed script

--- scripts/run_train_ner_PLMarker.sh
+++ scripts/run_train_ner_PLMarker.sh
@@ -27,7 +27,7 @@ CUDA_VISIBLE_DEVICES=$GPU_ID  python3  run_acener.py  --model_type bertspanmarke
     --model_name_or_path  bert_models/scibert-uncased  --do_lower_case  \
     --data_dir scierc  \
     --learning_rate 2e-5  --num_train_epochs 50  --per_gpu_train_batch_size  8  --per_gpu_eval_batch_size 16  --gradient_accumulation_steps 1  \
-    --max_seq_length 512  --save_steps 2000  --max_pair_length 256  --max_mention_ori_length 8    \
+    --max_seq_length 128  --save_steps 2000  --max_pair_length 256  --max_mention_ori_length 8    \
     --do_train  --do_eval  --evaluate_during_training   --eval_all_checkpoints  \
     --fp16  --seed $seed  --onedropout  --lminit  \
     --train_file train.json --dev_file dev.json --test_file test.json  \
@@ -37,7 +37,7 @@ done;
 python3 sumup.py sciner PL-Marker-ace05-bert
 
 
-# ACE04
+# # ACE04
 mkdir ace04ner_models
 for data_spilt in 0 1 2 3 4; do 
 CUDA_VISIBLE_DEVICES=$GPU_ID  python3  run_acener.py  --model_type bertspanmarker  \
@@ -61,7 +61,7 @@ CUDA_VISIBLE_DEVICES=$GPU_ID  python3  run_ner.py  --model_type robertaspanmarke
     --model_name_or_path  bert_models/roberta-large    \
     --data_dir conll03  \
     --learning_rate 1e-5  --num_train_epochs 8  --per_gpu_train_batch_size  4  --per_gpu_eval_batch_size 8  --gradient_accumulation_steps 2  \
-    --max_seq_length 512  --save_steps 2000  --max_pair_length 256  --max_mention_ori_length 8  \
+    --max_seq_length 128  --save_steps 2000  --max_pair_length 256  --max_mention_ori_length 8  \
     --do_train  --do_eval  --evaluate_during_training   --eval_all_checkpoints  \
     --fp16  --seed $seed  --onedropout  --lminit  \
     --train_file eng.train --dev_file eng.testa --test_file eng.testb  \
